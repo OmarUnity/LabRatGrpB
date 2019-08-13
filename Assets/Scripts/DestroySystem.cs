f92cee3d3f9cebd4ec04f2d99d075c6e3b960b90
Pablo Mosquera <juanpablo@unity3d.com>
2019-08-13 18:11:34 -0500

    DOTS pakages, Fall and Destroy System
    Import DOTS packages and simple fall and destroy systems

---
+++ Assets/Scripts/DestroySystem.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using Unity.Burst;
+using Unity.Collections;
+using Unity.Entities;
+using Unity.Jobs;
+using Unity.Mathematics;
+using Unity.Transforms;
+using UnityEngine;
+using Random = Unity.Mathematics.Random;
+
+public struct DestroyTag : IComponentData
+{
+}
+
+// This system updates all entities in the scene with both a RotationSpeed_SpawnAndRemove and Rotation component.
+public class DestroySystem : JobComponentSystem
+{
+    EntityCommandBufferSystem m_Barrier;
+
+    protected override void OnCreate()
+    {
+        m_Barrier = World.GetOrCreateSystem<EndSimulationEntityCommandBufferSystem>();
+    }
+
+    // Use the [BurstCompile] attribute to compile a job with Burst.
+    // You may see significant speed ups, so try it!
+    [BurstCompile]
+    struct RotationSpeedJob : IJobForEachWithEntity<DestroyTag>
+    {
+        //public float DeltaTime;
+
+        [WriteOnly]
+        public EntityCommandBuffer.Concurrent CommandBuffer;
+
+        public void Execute(Entity entity, int jobIndex, [ReadOnly] ref DestroyTag destroyTag)
+        {
+            CommandBuffer.DestroyEntity(jobIndex, entity);
+        }
+    }
+
+    // OnUpdate runs on the main thread.
+    protected override JobHandle OnUpdate(JobHandle inputDependencies)
+    {
+        var commandBuffer = m_Barrier.CreateCommandBuffer().ToConcurrent();
+
+        var job = new RotationSpeedJob
+        {
+            //DeltaTime = Time.deltaTime,
+            CommandBuffer = commandBuffer,
+
+        }.Schedule(this, inputDependencies);
+
+        m_Barrier.AddJobHandleForProducer(job);
+
+        return job;
+    }
+}
---
+++ Assets/Scripts/FallSystem.cs
@@ -0,0 +1,59 @@
+﻿using Unity.Burst;
+using Unity.Collections;
+using Unity.Entities;
+using Unity.Jobs;
+
+using UnityEngine;
+
+public struct FallTime : IComponentData
+{
+    public float Value;
+}
+
+// This system updates all entities in the scene with both a RotationSpeed_SpawnAndRemove and Rotation component.
+public class FallSystem : JobComponentSystem
+{
+    EntityCommandBufferSystem m_Barrier;
+
+    protected override void OnCreate()
+    {
+        m_Barrier = World.GetOrCreateSystem<EndSimulationEntityCommandBufferSystem>();
+    }
+
+    // Use the [BurstCompile] attribute to compile a job with Burst.
+    [BurstCompile]
+    struct FallTimeJob : IJobForEachWithEntity<FallTime>
+    {
+        public float DeltaTime;
+
+        [WriteOnly]
+        public EntityCommandBuffer.Concurrent CommandBuffer;
+
+        public void Execute(Entity entity, int jobIndex, ref FallTime fallTime)
+        {
+            fallTime.Value -= DeltaTime;
+
+            if (fallTime.Value < 0.0f)
+            {
+                CommandBuffer.AddComponent(jobIndex,entity,new DestroyTag());
+            }
+        }
+    }
+
+    // OnUpdate runs on the main thread.
+    protected override JobHandle OnUpdate(JobHandle inputDependencies)
+    {
+        var commandBuffer = m_Barrier.CreateCommandBuffer().ToConcurrent();
+
+        var job = new FallTimeJob
+        {
+            DeltaTime = Time.deltaTime,
+            CommandBuffer = commandBuffer,
+
+        }.Schedule(this, inputDependencies);
+
+        m_Barrier.AddJobHandleForProducer(job);
+
+        return job;
+    }
+}
