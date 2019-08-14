616468b95e167b71d9f69eb163f27b6416263fa0
Victor Riascos (Unity tech) <victorr@unity3d.com>
2019-08-14 10:33:36 -0500

    Fix move authoring (Due to some tags change their name)

--- Assets/Scripts/Authoring/Movement_Authoring.cs
+++ Assets/Scripts/Authoring/Movement_Authoring.cs
@@ -14,13 +14,13 @@ public class Movement_Authoring : MonoBehaviour, IConvertGameObjectToEntity
     public void Convert(Entity entity, EntityManager dstManager, GameObjectConversionSystem conversionSystem)
     {
         if ( direction == MovementDirection.Up )
-            dstManager.AddComponentData( entity, new LbUpDirection() );
+            dstManager.AddComponentData( entity, new LbNorthDirection() );
         if (direction == MovementDirection.Down)
-            dstManager.AddComponentData( entity, new LbDownDirection() );
+            dstManager.AddComponentData( entity, new LbSouthDirection() );
         if (direction == MovementDirection.Right)
-            dstManager.AddComponentData( entity, new LbRightDirection() );
+            dstManager.AddComponentData( entity, new LbEastDirection() );
         if (direction == MovementDirection.Left)
-            dstManager.AddComponentData( entity, new LbLeftDirection() );
+            dstManager.AddComponentData( entity, new LbWestDirection() );
 
         dstManager.AddComponentData( entity, new LbMovementSpeed { Value = speed } );
     }
