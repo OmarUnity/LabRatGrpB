4fca4a6deed05c772aea5ff844e20bb320946ade
rauloses <raul.oses@gmail.com>
2019-08-13 19:38:14 -0500

    Added remaining components

---
+++ Assets/Scripts/Component/LbScore.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Unity.Entities;
+
+/// <summary>
+/// Lifetime value, when 0 the entity is marked for destruction
+/// </summary>
+[Serializable]
+public struct LbScore : IComponentData
+{
+    public static byte kLbScoreType_RemovePoint = 0x00;
+    public static byte kLbScoreType_AddPoint = 0x01;
+    
+    /// <summary>
+    /// Score type
+    /// </summary>
+    public byte Type;
+
+    /// <summary>
+    /// Identifier for the player that will add or reduce score points
+    /// </summary>
+    public byte PlayerId;
+}
