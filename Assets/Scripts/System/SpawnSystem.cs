3df325b83bdb699ba8528b93bf928f51038222f1
Omar Calero <omar@unity3d.com>
2019-08-14 16:44:35 -0500

    Spawned entity now has LbDirection
    Spawned entity now has LbDirection

--- Assets/Scripts/System/SpawnSystem.cs
+++ Assets/Scripts/System/SpawnSystem.cs
@@ -2,6 +2,7 @@
 using Unity.Jobs;
 using Unity.Transforms;
 using UnityEngine;
+using Random = Unity.Mathematics.Random;
 
 public class SpawnSystem : JobComponentSystem
 {
@@ -16,12 +17,13 @@ public class SpawnSystem : JobComponentSystem
     {
         public EntityCommandBuffer.Concurrent CommandBuffer;
         public float DeltaTime;
-        
-        
+
         public void Execute(Entity entity, int index, ref LbSpawner lbSpawner, ref Translation translation, ref Rotation rotation)
         {
             lbSpawner.ElapsedTimeForMice += DeltaTime;
             lbSpawner.ElapsedTimeForCats += DeltaTime;
+            var random = new Random(1).NextInt(0, 3);
+            Debug.Log(random);
             
             if (lbSpawner.ElapsedTimeForMice > lbSpawner.MouseFrequency)
             {
@@ -30,7 +32,22 @@ public class SpawnSystem : JobComponentSystem
                 CommandBuffer.SetComponent(index, mouseInstance, new Translation{Value = translation.Value});
                 CommandBuffer.SetComponent(index, mouseInstance, new Rotation{Value = rotation.Value});
                 CommandBuffer.AddComponent<LbReachCell>(index, mouseInstance);
-                //CommandBuffer.AddComponent<>(index, mouseInstance);
+                if (random == 0)
+                {
+                    CommandBuffer.AddComponent<LbNorthDirection>(index, mouseInstance);
+                }
+                else if (random == 1)
+                {
+                    CommandBuffer.AddComponent<LbSouthDirection>(index, mouseInstance);
+                }
+                else if (random == 2)
+                {
+                    CommandBuffer.AddComponent<LbEastDirection>(index, mouseInstance);
+                }
+                else
+                {
+                    CommandBuffer.AddComponent<LbWestDirection>(index, mouseInstance);
+                }
             }
             
             if (lbSpawner.ElapsedTimeForCats > lbSpawner.CatFrequency)
@@ -40,7 +57,22 @@ public class SpawnSystem : JobComponentSystem
                 CommandBuffer.SetComponent(index, catInstance, new Translation{Value = translation.Value});
                 CommandBuffer.SetComponent(index, catInstance, new Rotation{Value = rotation.Value});
                 CommandBuffer.AddComponent<LbReachCell>(index, catInstance);
-                //CommandBuffer.AddComponent<>(index, catInstance);
+                if (random == 0)
+                {
+                    CommandBuffer.AddComponent<LbNorthDirection>(index, catInstance);
+                }
+                else if (random == 1)
+                {
+                    CommandBuffer.AddComponent<LbSouthDirection>(index, catInstance);
+                }
+                else if (random == 2)
+                {
+                    CommandBuffer.AddComponent<LbEastDirection>(index, catInstance);
+                }
+                else
+                {
+                    CommandBuffer.AddComponent<LbWestDirection>(index, catInstance);
+                }
             }
         }
     }
