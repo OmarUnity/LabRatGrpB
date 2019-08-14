a591e1389623b999b6d0247854db2c1dad16dd24
Sergio Gomez <sergioggomez@gmail.com>
2019-08-14 10:13:23 -0500

    Add DirectionMap Buffer

---
+++ Assets/Scripts/Component/LbDirectionMap.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Unity.Entities;
+
+[InternalBufferCapacity(100)]
+public struct LbDirectionMap : IBufferElementData
+{
+    // These implicit conversions are optional, but can help reduce typing.
+    public static implicit operator short(LbDirectionMap e) { return e.Value; }
+    public static implicit operator LbDirectionMap(short e) { return new LbDirectionMap { Value = e }; }
+
+    // Actual value each buffer element will store.
+    public short Value;
+}
