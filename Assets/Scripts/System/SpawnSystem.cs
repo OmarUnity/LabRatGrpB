c85eeabfe5ebaa730a5162d0ee2faaf03a0a10cf
Omar Calero <omar@unity3d.com>
2019-08-14 16:27:19 -0500

    Spawned cats or mice now have LbReachCell component
    Spawned cats or mice now have LbReachCell component

--- Assets/Scripts/System/SpawnSystem.cs
+++ Assets/Scripts/System/SpawnSystem.cs
@@ -17,6 +17,7 @@ public class SpawnSystem : JobComponentSystem
         public EntityCommandBuffer.Concurrent CommandBuffer;
         public float DeltaTime;
         
+        
         public void Execute(Entity entity, int index, ref LbSpawner lbSpawner, ref Translation translation, ref Rotation rotation)
         {
             lbSpawner.ElapsedTimeForMice += DeltaTime;
@@ -28,6 +29,8 @@ public class SpawnSystem : JobComponentSystem
                 var mouseInstance = CommandBuffer.Instantiate(index, lbSpawner.MousePrefab);
                 CommandBuffer.SetComponent(index, mouseInstance, new Translation{Value = translation.Value});
                 CommandBuffer.SetComponent(index, mouseInstance, new Rotation{Value = rotation.Value});
+                CommandBuffer.AddComponent<LbReachCell>(index, mouseInstance);
+                //CommandBuffer.AddComponent<>(index, mouseInstance);
             }
             
             if (lbSpawner.ElapsedTimeForCats > lbSpawner.CatFrequency)
@@ -36,6 +39,8 @@ public class SpawnSystem : JobComponentSystem
                 var catInstance = CommandBuffer.Instantiate(index, lbSpawner.CatPrefab);
                 CommandBuffer.SetComponent(index, catInstance, new Translation{Value = translation.Value});
                 CommandBuffer.SetComponent(index, catInstance, new Rotation{Value = rotation.Value});
+                CommandBuffer.AddComponent<LbReachCell>(index, catInstance);
+                //CommandBuffer.AddComponent<>(index, catInstance);
             }
         }
     }
