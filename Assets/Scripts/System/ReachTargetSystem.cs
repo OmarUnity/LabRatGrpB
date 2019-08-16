8cecb9b9df9541c8f22c79e196dae936c759b6d4
Raul Oses Lopez <raul.oses@gmail.com>
2019-08-16 12:59:05 -0500

    Improved ReachTargetSystem to use Burst and Queues

--- Assets/Scripts/System/ReachTargetSystem.cs
+++ Assets/Scripts/System/ReachTargetSystem.cs
@@ -1,5 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using Unity.Burst;
+using Unity.Collections;
 using UnityEngine;
 using Unity.Entities;
 using Unity.Jobs;
@@ -7,18 +9,34 @@ using Unity.Mathematics;
 
 public class ReachTargetSystem : JobComponentSystem
 {
-    EntityCommandBufferSystem m_Barrier;
+    private EntityCommandBufferSystem m_Barrier;
+    private NativeQueue<Entity> m_Queue;
 
+    [BurstCompile]
     public struct ReachTargetJob : IJobForEachWithEntity<LbDistanceToTarget>
     {
-        public EntityCommandBuffer.Concurrent commandBuffer;
+        public NativeQueue<Entity>.ParallelWriter Queue;
 
         public void Execute(Entity entity, int index, ref LbDistanceToTarget distanceToTarget)
         {
-            if ( distanceToTarget.Value == 0 )
-            {
-                commandBuffer.AddComponent( index, entity, new LbReachCell() );
+            if (distanceToTarget.Value <= 0.00001f)
+            {   
                 distanceToTarget.Value = 1.0f;
+                Queue.Enqueue(entity);
+            }
+        }
+    }
+
+    public struct ReachTargetCleanJob : IJob
+    {
+        public NativeQueue<Entity> Queue;
+        public EntityCommandBuffer CommandBuffer;
+        
+        public void Execute()
+        {
+            while (Queue.Count > 0)
+            {
+                CommandBuffer.AddComponent(Queue.Dequeue(), new LbReachCell());
             }
         }
     }
@@ -26,16 +44,28 @@ public class ReachTargetSystem : JobComponentSystem
     protected override void OnCreate()
     {
         m_Barrier = World.Active.GetOrCreateSystem<LbSimulationBarrier>();
+        m_Queue = new NativeQueue<Entity>(Allocator.Persistent);
+    }
+
+    protected override void OnDestroy()
+    {
+        m_Queue.Dispose();
     }
 
     protected override JobHandle OnUpdate(JobHandle inputDeps)
     {
-        var job = new ReachTargetJob {
-            commandBuffer = m_Barrier.CreateCommandBuffer().ToConcurrent()
+        var handle = new ReachTargetJob {
+            Queue = m_Queue.AsParallelWriter(),
         }.Schedule(this, inputDeps);
 
-        m_Barrier.AddJobHandleForProducer( job );
+        handle = new ReachTargetCleanJob()
+        {
+            Queue = m_Queue,
+            CommandBuffer = m_Barrier.CreateCommandBuffer()
+        }.Schedule(handle);
+        
+        m_Barrier.AddJobHandleForProducer( handle );
 
-        return job;
+        return handle;
     }
 }
